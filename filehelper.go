6eb7617052c9173709ed81ff36ffbfc758287893
beeleelee <beeleelee@outlook.com>
2021-10-28 13:31:58 +0800

    feat:add RandPort

--- filehelper.go
+++ filehelper.go
@@ -1,6 +1,23 @@
 package filehelper
 
-import "os"
+import (
+	"fmt"
+	mrand "math/rand"
+	"os"
+	"time"
+)
+
+const (
+	RandPortMin = 4000
+	RandPortMax = 65000
+)
+
+func RandPort() string {
+	mrand.Seed(time.Now().Unix() * int64(mrand.Intn(RandPortMax)))
+	r := mrand.Float64()
+	m := RandPortMin + (RandPortMax-RandPortMin)*r
+	return fmt.Sprintf("%.0f", m)
+}
 
 func ExistDir(path string) bool {
 	s, err := os.Stat(path)
